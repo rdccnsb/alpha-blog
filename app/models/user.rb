9c9acdd6f5b9f4a349c490e1083885650dbc1890
Rails Udemy Course <roger_carb@hotmail.com>
2016-10-12 14:58:59 +0000

    add user info in view and user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  has_many  :articles
+  before_save { self.email = email.downcase }
   validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX } 
+  has_secure_password
 end
